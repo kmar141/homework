eda9bbeadabdd4b6bd8de8c679eb2b140e0b9e77
Kieran Marshall <youthassassin@gmail.com>
2017-05-23 23:07:27 +0100

    updated ecosystem files

--- ecosystem/ecosystem.rb
+++ ecosystem/ecosystem.rb
@@ -2,9 +2,53 @@ class Bear
 
   def initialize(name)
     @name = name
+    @stomach = []
   end
 
   def bear_name()
+    return @name
+  end
+
+  def bear_stomach_contents()
+    return @stomach.count()
+  end
+
+  def bear_eats_fish(fish)
+    @stomach << fish
+  end
+
+  def bear_roar
+    return "Pic-a-nic baskets!"
+  end
+end
+
+class River
+
+  def initialize(name)
+    @name = name
+    @fish = []
+  end
+
+  def river_name()
+     return @name
+  end
+
+  def fish_in_river()
+    return @fish.count()
+  end
+
+  def fish_eaten()
+  end
+
+end
+
+class Fish
+
+  def initialize(name)
+    @name = name
+  end
+
+  def fish_name()
     return @name
   end
 
--- ecosystem/specs/ecosystem_spec.rb
+++ ecosystem/specs/ecosystem_spec.rb
@@ -5,13 +5,41 @@ require_relative "../ecosystem"
 
 class TestEcosystem < Minitest::Test
   def setup()
-    # @amazon = River.new("Amazon River", fish)
+    @amazon = River.new("Amazon River")
     @grizzly = Bear.new("Grizzly Bear")
-    # @goldy = Fish.new("Goldfish")
+    @goldy = Fish.new("Goldfish")
+    @blinky = Fish.new("Blinky the Three-Eyed Fish")
   end
 
   def test_bear_name()
-    assert_equal("Grizzly Bear", @grizzly)
+    assert_equal("Grizzly Bear", @grizzly.bear_name())
+    assert_equal("Amazon River", @amazon.river_name())
+    assert_equal("Goldfish", @goldy.fish_name())
+    assert_equal("Blinky the Three-Eyed Fish", @blinky.fish_name)
   end
+
+  def test_bear_eats_fish()
+    @grizzly.bear_eats_fish(@goldy)
+    assert_equal(1, @grizzly.bear_stomach_contents())
+  end
+
+  def test_bear_roar()
+    assert_equal("Pic-a-nic baskets!", @grizzly.bear_roar)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
